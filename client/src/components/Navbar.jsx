82268569183649e6ec1a395a80b6463065061471
Sithandekile Ndhlovu <152465186+sithandekile@users.noreply.github.com>
2026-09-19 17:59:05 +0200

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, UserButton } from '@clerk/react';
 import ThemeToggle from './themeToggle';
 
 export default function Navbar() {
